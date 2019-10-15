837a9d4fcc6019eb8fbdcff8b09dbfcc8f85b735
adi <52442230+adi-lazaro@users.noreply.github.com>
2019-10-15 12:54:24 +0530

    Tracking / Analytics : Add Google Tag Manager (GTM) embed snippets

--- inc/above.php
+++ inc/above.php
@@ -36,6 +36,10 @@ http_response_code( 200 );
 
 	<body id="body" class="body">
 
+		<!-- Google Tag Manager (noscript) -->
+		<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WN3Z6CT" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+		<!-- End Google Tag Manager (noscript) -->
+
 		<?php
 			/*
 			 * Arbitrary Code ( Top of Body )
