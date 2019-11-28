8ad88e62d351f8c330d28dddcd87fa530a3fc26e
MarioWindsor <mario@lazaro.in>
2019-11-28 15:38:12 +0530

    Front-end Build: What is Included Modal

--- inc/modals.php
+++ inc/modals.php
@@ -34,11 +34,133 @@
 				</div>
 				<div class="row">
 					<div class="columns small-12">
-						<p>Augmented reality chrome network skyscraper Tokyo camera military-grade cardboard footage ablative otaku warehouse Kowloon table tower monofilament. Bicycle girl tower network face forwards towards fetishism corporation tiger-team. Monofilament decay hacker RAF dolphin DIY franchise narrative math-skyscraper realism systemic order-flow corrupted. Math-sentient tube cyber-paranoid order-flow long-chain hydrocarbons Chiba boy. RAF advert narrative dissident car wristwatch soul-delay cardboard nano-neon silent. Wonton soup pistol nano-otaku assault franchise realism RAF denim skyscraper geodesic tube into weathered youtube artisanal grenade. Youtube monofilament smart-corporation military-grade Tokyo meta-papier-mache corrupted disposable plastic savant shanty town dolphin 8-bit wonton soup. Military-grade Tokyo digital 3D-printed boat advert San Francisco engine tattoo computer skyscraper physical construct. Sub-orbital computer media market order-flow nodal point j-pop spook Chiba soul-delay tiger-team tanto cartel. Camera tower-space franchise range-rover futurity network military-grade Shibuya. Saturation point tanto physical BASE jump 3D-printed neural fetishism long-chain hydrocarbons rain. Bicycle apophenia futurity digital boat denim post-jeans free-market car corporation range-rover cardboard convenience store concrete. Long-chain hydrocarbons j-pop Tokyo crypto-table cardboard render-farm. Tokyo boy disposable industrial grade bridge A.I. carbon footage BASE jump cartel free-market euro-pop long-chain hydrocarbons-ware grenade. Pre-man voodoo god towards euro-pop cyber-crypto-Legba systema modem beef noodles. Free-market boy sensory post--space systemic jeans. Uplink singularity shanty town voodoo god rifle tank-traps smart-katana shrine human. Hotdog savant human garage wonton soup tube dolphin j-pop. Tiger-team wristwatch engine vehicle cartel apophenia augmented reality man network stimulate. Shrine claymore mine monofilament hotdog voodoo god geodesic knife. Voodoo god silent otaku hacker computer post-singularity office tower shanty town. Tokyo lights corrupted marketing skyscraper receding beef noodles uplink footage gang rebar order-flow table nano-bicycle tube. </p>
+						<p>Augmented reality chrome network skyscraper Tokyo camera military-grade cardboard footage ablative otaku warehouse Kowloon table tower monofilament. Bicycle girl tower network face forwards towards fetishism corporation tiger-team. Monofilament decay hacker RAF dolphin DIY franchise narrative math-skyscraper realism systemic order-flow corrupted. Math-sentient tube cyber-paranoid order-flow long-chain hydrocarbons Chiba boy. RAF advert narrative dissident car wristwatch soul-delay cardboard nano-neon silent. Wonton soup pistol nano-otaku assault franchise realism RAF denim skyscraper geodesic tube into weathered youtube artisanal grenade. Youtube monofilament smart-corporation military-grade Tokyo meta-papier-mache corrupted disposable plastic savant shanty town dolphin 8-bit wonton soup. Military-grade Tokyo digital 3D-printed boat advert San Francisco engine tattoo computer skyscraper physical construct. Sub-orbital computer media market order-flow nodal point j-pop spook Chiba soul-delay tiger-team tanto cartel. Camera tower-space franchise range-rover futurity network military-grade Shibuya. Saturation point tanto physical BASE jump 3D-printed neural fetishism long-chain hydrocarbons rain. Bicycle apophenia futurity digital boat denim post-jeans free-market car corporation range-rover cardboard convenience store concrete. Long-chain hydrocarbons j-pop Tokyo crypto-table cardboard render-farm. Tokyo boy disposable industrial grade bridge A.I. carbon footage BASE jump cartel free-market euro-pop long-chain hydrocarbons-ware grenade. Pre-man voodoo god towards euro-pop cyber-crypto-Legba systema modem beef noodles. Free-market boy sensory post--space systemic jeans. Uplink singularity shanty town voodoo god rifle tank-traps smart-katana shrine human. Hotdog savant human garage wonton soup tube dolphin j-pop. Tiger-team wristwatch engine vehicle cartel apophenia augmented reality man network stimulate. Shrine claymore mine monofilament hotdog voodoo god geodesic knife. Voodoo god silent otaku hacker computer post-singularity office tower shanty town. Tokyo lights corrupted marketing skyscraper receding beef noodles uplink footage gang rebar order-flow table nano-bicycle tube. </span>
 					</div>
 				</div>
 			</div>
 		</div><!-- END : Sample Form -->
+
+		<!-- Modal Content : What is Included -->
+		<style type="text/css">
+			.what-is-included {}
+			@media( min-width: 1040px ) {
+				.what-is-included .column-wrap {
+					columns: 2;
+					column-gap: var(--space-50);
+				}
+			}
+		</style>
+		<div class="modal-box-content js_modal_box_content" data-mod-id="what-is-included">
+			<div class="container">
+				<div class="row what-is-included">
+					<div class="columns small-12 fill-light" style="border-radius: 10px; overflow: hidden;">
+						<div class="row space-50-top space-min-bottom">
+							<div class="columns small-10 small-offset-1 large-8 xlarge-7">
+								<div class="h3 strong">The <span class="text-green-2">Buddy</span> Package</div>
+								<div class="h2 strong space-min-bottom text-green-2" style="margin-left: calc(var(--space-25) * -1); line-height: 1;">@Alta Vista BLR</div>
+								<div class="h4 strong">For a monthly fee of <span class="text-green-2">₹11,200</span></div>
+								<div class="h4 strong space-25-top">Gives you access to a Twin Sharing room in a furnished 3 Bedroom Suite. Your room has an attached bathroom and an attached Balcony.</div>
+							</div>
+							<div class="columns small-10 small-offset-1 large-8 xlarge-7">
+								<div class="h4 strong space-25-top-bottom text-green-2">What is in your room?</div>
+							</div>
+							<div class="columns small-10 small-offset-1 large-8 xlarge-7 column-wrap">
+								<span class="block space-min-bottom">
+									<div class="p">2 Beds with mattress, pillow, 2 sets of linen</div>
+									<div class="p">2 Private Cupboards</div>
+									<div class="p">Curtains</div>
+									<div class="p">Trash Bin</div>
+								</span>
+								<span class="block space-min-bottom">
+									<div class="p strong text-uppercase">Attached Bathroom</div>
+									<div class="p">- Geyser</div>
+									<div class="p">- Shower Cubicle</div>
+									<div class="p">- Hand Shower</div>
+									<div class="p">- Vanity Basin</div>
+									<div class="p">- Western Toilet </div>
+									<div class="p">- Bathroom Faucet</div>
+									<div class="p">- Towel Rack</div>
+									<div class="p">- Soap Stand</div>
+									<div class="p">- Wall Mounted Mirror</div>
+									<div class="p">- Bathroom Essentials (Bucket & Mug)</div>
+									<div class="p">- Door Mat</div>
+								</span>
+								<span class="block space-min-bottom">
+									<div class="p strong text-uppercase">Attached Balcony </div>
+								</span>
+							</div>
+							<div class="columns small-10 small-offset-1 large-8 xlarge-7">
+								<div class="h4 strong space-25-top-bottom text-green-2">What is in your suite?</div>
+							</div>
+							<div class="columns small-10 small-offset-1 large-8 xlarge-7 column-wrap">
+								<span class="block space-min-bottom">
+									<div class="p">The suite is a 1800sft 3BHK with a living room, dining room, kitchen and utility area. The suite is furnished.</div>
+								</span>
+								<span class="block space-min-bottom">
+									<div class="p strong text-uppercase">Living Room</div>
+									<div class="p">Sofa with Centre Table </div>
+									<div class="p">TV with Stand</div>
+									<div class="p">Wall Paintings</div>
+									<div class="p">Curtains</div>
+									<div class="p">Lights</div>
+									<div class="p">Fans</div>
+									<div class="p">Door Mat</div>
+									<div class="p">Wall Clock</div>
+								</span>
+								<span class="block space-min-bottom">
+									<div class="p strong text-uppercase">Dining Room</div>
+									<div class="p">Dining / Work Table </div>
+									<div class="p">6 Chairs</div>
+									<div class="p">Curtains</div>
+									<div class="p">Lights </div>
+									<div class="p">Fans</div>
+								</span>
+								<span class="block space-min-bottom">
+									<div class="p strong text-uppercase">Kitchen</div>
+									<div class="p">Refrigerator</div>
+									<div class="p">Microwave Oven</div>
+									<div class="p">Induction Stove</div>
+									<div class="p">Gas Stove</div>
+									<div class="p">Sandwich Toaster</div>
+									<div class="p">Water Purifier</div>
+									<div class="p">Electric Cooker</div>
+									<div class="p">Kitchen Utensils</div>
+									<div class="p">Electric Kettle</div>
+									<div class="p">Electric Waste Disposal Churner </div>
+								</span>
+								<span class="block space-min-bottom">
+									<div class="p strong text-uppercase">Utility</div>
+									<div class="p">Washing Machine</div>
+									<div class="p">Electric Clothes Dryer</div>
+									<div class="p">Trash Bin</div>
+								</span>
+							</div>
+							<div class="columns small-10 small-offset-1 large-8 xlarge-7">
+								<div class="h4 strong space-25-top-bottom text-green-2">Services that are included?</div>
+								<span class="block space-min-bottom">
+									<div class="p">DTH </div>
+									<div class="p">High Speed Internet</div>
+									<div class="p">Wifi</div>
+									<div class="p">Housekeeping</div>
+									<div class="p">Water Supply</div>
+									<div class="p">Electricity Supply </div>
+									<div class="p">24/7 Power Backup</div>
+								</span>
+							</div>
+						</div>
+						<div class="row fill-dark space-min-top space-50-bottom" style="background: linear-gradient(15deg, var(--dark) 20%, var(--neutral-4) 100%);">
+							<div class="columns small-10 small-offset-1 large-8 xlarge-7">
+								<div class="h4 strong space-25-top-bottom">Optional Add-on Services</div>
+								<span class="block space-min-bottom">
+									<div class="h5">2 meals a day - <span class="text-green-2">Monthly Fee of ₹2,300</span></div>
+									<div class="h5">Weekly Laundry - <span class="text-green-2">Monthly Fee of ₹1,200</span></div>
+								</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div><!-- END : What is Included -->
 
 
 		<!-- Modal Close Button -->
