d234f25ed4edc41b72739393246f994adbcbd7f3
adi <52442230+adi-lazaro@users.noreply.github.com>
2019-12-16 16:18:18 +0530

    Utilities : Add a scroll handler hub and a "debounce" and "throttle" function

--- js/modules/utils.js
+++ js/modules/utils.js
@@ -13,3 +13,91 @@ function waitFor ( seconds ) {
 		}, seconds * 1000 );
 	} );
 }
+
+
+
+/*
+ *
+ * Return a debounced version of a given function
+ *
+ */
+function getDebouncedVersion ( fn, debounceBy ) {
+
+	debounceBy = ( debounceBy || 1 ) * 1000;
+
+	var timeoutId;
+	var rafId;
+
+	return function () {
+		window.clearTimeout( timeoutId );
+		timeoutId = window.setTimeout( function () {
+			window.cancelAnimationFrame( rafId );
+			rafId = window.requestAnimationFrame( fn );
+		}, debounceBy );
+	};
+
+}
+
+
+/*
+ *
+ * Return a throttled version of a given function
+ *
+ */
+
+function getThrottledVersion ( fn, throttleBy ) {
+
+	throttleBy = ( throttleBy || 1 ) * 1000;
+
+	var timeoutId;
+
+	function preparedFunction () {
+		fn();
+		timeoutId = null;
+	}
+
+	return function () {
+
+		if ( timeoutId )
+			return;
+
+		timeoutId = window.setTimeout( function () {
+			window.requestAnimationFrame( preparedFunction );
+		}, throttleBy );
+
+	};
+
+}
+
+
+
+
+/*
+ *
+ * Scroll Event Handling Hub
+ *
+ */
+var registeredScrollHandlers = [ ];
+function scrollHandler ( event ) {
+	var _i, _len = registeredScrollHandlers.length;
+	for ( _i = 0; _i < _len; _i += 1 ) {
+		try {
+			registeredScrollHandlers[ _i ]();
+		}
+		catch ( e ) {
+			console.log( e.message );
+			console.log( e.stack );
+		}
+	}
+}
+function onScroll ( handler, options ) {
+	options = options || { };
+	let preparedHandler = handler;
+	if ( options.behavior == "debounce" )
+		preparedHandler = getDebouncedVersion( handler, options.by );
+	else if ( options.behavior == "throttle" )
+		preparedHandler = getThrottledVersion( handler, options.by );
+
+	registeredScrollHandlers = registeredScrollHandlers.concat( preparedHandler );
+}
+window.addEventListener( "scroll", scrollHandler );
