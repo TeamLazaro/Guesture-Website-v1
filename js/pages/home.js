0038ddaaf3b13d0f82f991cc84ebdd47204ce9b3
adi <52442230+adi-lazaro@users.noreply.github.com>
2019-12-16 16:19:38 +0530

    Home Page : Animation : Organize better; reset animation when scrolling away

--- js/pages/home.js
+++ js/pages/home.js
@@ -2,12 +2,14 @@
 
 /*
  * -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
- *  Play the animations
+ *  The animations
  * -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-
  */
 $( function () {
 
-	// Build the animations
+	/*
+	 * Construct the animations
+	 */
 	var scoreOutAnimation = lottie.loadAnimation( {
 		container: $( ".js_score_out" ).get( 0 ),
 		renderer: "svg",
@@ -21,17 +23,50 @@ $( function () {
 		animationData: monthlyFee__AnimationData
 	} );
 
-	$( ".js_no_to_landlords" ).on( "reveal/post", function () {
-		waitFor( 1 )
-			.then( function () {
-				scoreOutAnimation.play();
-			} )
-			.then( function () {
-				return waitFor( 1 );
-			} )
-			.then( function () {
-				monthlyFeeAnimation.play();
-			} )
+
+	/*
+	 * Sequence the animations
+	 */
+	// Set the "Monthly Fee" animation to play after the "Score out" one
+	scoreOutAnimation.addEventListener( "complete", function () {
+		monthlyFeeAnimation.play()
 	} );
+	// Make a function that sets the animation in motion
+	function playAnimation () {
+		// If either of the animations are already playing, do nothing
+		if ( ! scoreOutAnimation.isPaused || ! monthlyFeeAnimation.isPaused )
+			return;
+
+		scoreOutAnimation.play();
+	}
+	function resetAnimation () {
+		scoreOutAnimation.goToAndStop( 0 );
+		monthlyFeeAnimation.goToAndStop( 0 );
+	}
+
+
+	/*
+	 * Configure when the animation should be played
+	 */
+	// When the section is revealed
+	$( ".js_no_to_landlords" ).one( "reveal/post", function () {
+		waitFor( 0.5 ).then( playAnimation );
+	} );
+
+	// When the section is scrolled to
+	var $noToLandlords = $( ".js_no_to_landlords" );
+	onScroll( function () {
+		var currentScrollTop = window.scrollY || document.body.scrollTop;
+		var elementBottom = $noToLandlords.position().top
+							+ $noToLandlords.height()
+							// arbitrary number (to compensate for the transform)
+							+ 150;
+		if ( currentScrollTop < elementBottom ) {
+			if ( scoreOutAnimation.currentFrame === 0 )
+				playAnimation();
+		}
+		else
+			resetAnimation();
+	}, { behavior: "throttle", by: 0.5 } );
 
 } );
