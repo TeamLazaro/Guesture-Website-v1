4a356b8b9dfbb78029e4cb6a74428c093a50e1b0
adi <52442230+adi-lazaro@users.noreply.github.com>
2020-02-03 15:56:54 +0530

    Deals : Login Prompts : Inject the markup for this via the PHP module

--- pages/home.php
+++ pages/home.php
@@ -718,46 +718,7 @@ if ( cmsIsEnabled() ) {
 								<span class="invisible label inline text-neutral-1 text-uppercase">Book Now</span>
 								<a class="button block js_book_trial" href="https://www.instamojo.com/guesture/3-day-trial-stay/" target="_blank" data-c="three-day-trial">Book Now</a>
 							</label>
-							<br>
-							<!-- Phone Trap form -->
-							<form class="js_phone_form" style="display: none">
-								<div class="columns small-12" style="position: relative">
-									<label><span class="label inline text-neutral-2 text-uppercase">Mobile Number</span></label>
-									<div class="phone-trap minimal phone-number">
-										<div class="block prefix-group" style="position: relative">
-											<select class="js_phone_country_code" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; opacity: 0">
-												<?php include __DIR__ . '/../inc/phone-country-codes.php' ?>
-											</select>
-											<input class="prefix js_phone_country_code_label button" value="+91" style="pointer-events: none; width: 100%;">
-										</div>
-										<input class="phone block" type="text" name="phone-number">
-										<label class="submit block">
-											<span class="hidden label inline text-neutral-2 text-uppercase">Submit</span>
-											<button class="button block">→</button>
-										</label>
-									</div>
-								</div>
-							</form>
-							<!-- OTP form -->
-							<form class="js_otp_form" style="display: none">
-								<div class="columns small-12">
-									<div class="otp-trap minimal">
-										<label class="block">
-											<span class="label inline text-neutral-2 text-uppercase">Enter the OTP</span>
-											<input class="otp block" type="text" name="otp">
-										</label>
-										<label class="submit block">
-											<span class="invisible label inline text-neutral-2 text-uppercase">Submit</span>
-											<button class="button block">→</button>
-										</label>
-									</div>
-								</div>
-								<br>
-								<div class="columns small-12 clearfix hidden">
-									<div class="label strong text-neutral-2 opacity-50 text-uppercase inline-middle cursor-pointer float-left" tabindex="-1">Re-send OTP</div>
-									<div class="label strong text-neutral-2 opacity-50 text-uppercase inline-middle cursor-pointer float-right" tabindex="-1">Enter different number</div>
-								</div>
-							</form>
+							<?php require __DIR__ . '/../inc/login-prompt.php'; ?>
 						</div>
 					</div>
 				</div>
