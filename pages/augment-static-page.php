e01917886f3cf27a087b9dde62cd24b1e660348f
adi <adi@lazaro.in>
2020-02-26 14:20:28 +0530

    Page Augmenter : Allow for multiple code snippets to be injected

--- pages/augment-static-page.php
+++ pages/augment-static-page.php
@@ -1,9 +1,16 @@
 <?php
 
 $pagePath = $_SERVER[ 'DOCUMENT_ROOT' ] . '/' . $_GET[ 'path' ];
-$augmentFilePath = __DIR__ . '/../inc/snippets/' . $_GET[ 'with' ] . '.php';
+$augmentSnippets = $_GET[ 'with' ];
+if ( ! is_array( $augmentSnippets ) )
+	$augmentSnippets = [ $augmentSnippets ];
+
+$augmentCode = '';
+foreach ( $augmentSnippets as $snippetName ) {
+	$snippetFilePath = __DIR__ . '/../inc/snippets/' . $snippetName . '.php';
+	$augmentCode .= file_get_contents( $snippetFilePath );
+}
 
 $pageCode = file_get_contents( $pagePath );
-$augmentCode = file_get_contents( $augmentFilePath );
 // Inject the snippet right before the closing body tag
 echo preg_replace( '/<\/body>\s*<\/html>\s*$/', $augmentCode, $pageCode ) . '</body></html>';
