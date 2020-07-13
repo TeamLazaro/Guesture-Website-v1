145fbf01c23e35ba141869b8fb2bb753d7013242
adi <adi@lazaro.in>
2020-07-13 10:34:24 +0530

    Library : Guesture API : Update this to account for refreshing of OAuth2 tokens

--- server/api/guesture.php
+++ server/api/guesture.php
@@ -11,14 +11,80 @@ class Guesture {
 
 
 
+	public static function getAccessToken () {
+
+		// Pull the token from the cache
+		require_once __DIR__ . '/guesture-api-conf.php';
+		$accessToken = GUESTURE_OAUTH_ACCESS_TOKEN;
+		$expiresAt = GUESTURE_OAUTH_ACCESS_TOKEN_EXPIRES_AT;
+
+		// If the token has not expired, simply return it
+		if ( ! empty( $accessToken ) and ( $expiresAt + 1 ) > time() )
+			return $accessToken;
+
+		// If the token has expired, or will expire within the second, then get a new one
+		while ( empty( $accessToken ) or ( $expiresAt + 1 ) < time() ) {
+			sleep( 1 );
+			$tokenResponse = self::requestAccessToken();
+			$accessToken = $tokenResponse[ 'access_token' ];
+			$expiresAt = time() + $tokenResponse[ 'expires_in' ];
+		}
+
+		self::cacheAccessToken( $accessToken, $expiresAt );
+
+		return $accessToken;
+
+	}
+
+	public static function requestAccessToken () {
+
+		$url = GUESTURE_OAUTH_SERVER;
+
+		$request = curl_init();
+		curl_setopt( $request, CURLOPT_URL, $url );
+		curl_setopt( $request, CURLOPT_RETURNTRANSFER, true );
+		curl_setopt( $request, CURLOPT_USERAGENT, 'Guestur' );
+		curl_setopt( $request, CURLOPT_CUSTOMREQUEST, 'POST' );
+		curl_setopt( $request, CURLOPT_HTTPHEADER, [
+			'Authorization: Basic MjFlNDNjNTUtMjhlZi00NzhhLWFlNjUtZGM4OTZlNWVhYTM0OlBAc3N3MHJk',
+			'Content-Type: application/x-www-form-urlencoded'
+		] );
+		curl_setopt( $request, CURLOPT_POSTFIELDS, 'grant_type=client_credentials' );
+
+		$response = curl_exec( $request );
+		curl_close( $request );
+
+		$body = json_decode( $response, true );
+
+		if ( empty( $body ) )
+			return [ ];
+
+		return $body;
+
+	}
+
+	public static function cacheAccessToken ( $accessToken, $expiresAt ) {
+		$credentialsFileContent = '<?php'
+						. PHP_EOL . PHP_EOL
+						. 'const GUESTURE_OAUTH_ACCESS_TOKEN = \'' . $accessToken . '\';'
+						. PHP_EOL
+						. 'const GUESTURE_OAUTH_ACCESS_TOKEN_EXPIRES_AT = ' . $expiresAt . ';'
+						. PHP_EOL;
+		file_put_contents( __DIR__ . '/guesture-api-conf.php', $credentialsFileContent );
+	}
+
+
+
 	public static function httpRequest ( $endpoint, $method = 'GET', $data = [ ] ) {
+
+		$accessToken = self::getAccessToken();
 
 		$request = curl_init();
 		curl_setopt( $request, CURLOPT_URL, $endpoint );
 		curl_setopt( $request, CURLOPT_RETURNTRANSFER, true );
 		curl_setopt( $request, CURLOPT_USERAGENT, 'Guestur' );
 		$headers = [
-			'Authorization: Bearer ' . GUESTURE_OAUTH_BEARER_TOKEN,
+			'Authorization: Bearer ' . $accessToken,
 			'Cache-Control: no-cache, no-store, must-revalidate'
 		];
 		if ( ! empty( $data ) and is_array( $data ) ) {
@@ -53,7 +119,7 @@ class Guesture {
 
 		$requestBody = [
 			'orderId' => null,
-			'stayPackage' => $data[ 'type' ],
+			'stayPackage' => $data[ 'stayPackage' ],
 			'property' => $data[ 'location' ],
 			'hasBalcony' => $data[ 'hasBalcony' ],
 			'hasBathRoom' => $data[ 'hasBathroom' ],	// Yes, the capital 'R' in the field name is important
@@ -66,8 +132,8 @@ class Guesture {
 			'selectedAmenities' => null,
 			'checkInDate' => $data[ 'fromDate' ],
 			'checkOutDate' => $data[ 'toDate' ],
-			'stayDuration' => $data[ 'duration' ],
-			'stayDurationUnit' => 1
+			'stayDuration' => $data[ 'durationAmount' ],
+			'stayDurationUnit' => $data[ 'durationUnit' ] === 'days' ? 2 : 1
 		];
 
 		$responseBody = self::httpRequest( $endpoint, 'POST', $requestBody );
